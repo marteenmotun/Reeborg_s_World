09b3c49342f1183c4f6efa4524c4279f8d3df792
motun <motunmarteen@gmail.com>
2023-02-08 11:55:34 +0100

    Around the world

---
+++ Around 1.py
@@ -0,0 +1,16 @@
+def line_1():
+    repeat 9:
+        move()
+
+def line_2():
+    turn_left()
+    repeat 9:
+        move()
+        
+line_1()
+repeat 3:
+    line_2()
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
